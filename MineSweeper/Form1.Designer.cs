cbe11f96f11dc81061b64b748f3c8655670e866e
Connor Stewart <cjs2002a@gmail.com>
2020-05-28 22:35:49 -0400

    Add project files.

---
+++ MineSweeper/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MineSweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_BombsRemaining = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(48, 40);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(600, 600);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bombs Left: ";
+            // 
+            // label_BombsRemaining
+            // 
+            this.label_BombsRemaining.AutoSize = true;
+            this.label_BombsRemaining.Location = new System.Drawing.Point(126, 9);
+            this.label_BombsRemaining.Name = "label_BombsRemaining";
+            this.label_BombsRemaining.Size = new System.Drawing.Size(0, 13);
+            this.label_BombsRemaining.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 666);
+            this.Controls.Add(this.label_BombsRemaining);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_BombsRemaining;
+    }
+}
+
